7b2191a5bfbbf952c7cebfb401a4167fd78d3d66
admin <admin@DESKTOP-89A4BSE>
2021-08-08 09:26:11 +0530

    Code??

--- src/main/java/com/demo/ExcelProject/ExcelIterator.java
+++ src/main/java/com/demo/ExcelProject/ExcelIterator.java
@@ -1,7 +1,6 @@
 package com.demo.ExcelProject;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 
--- src/main/java/com/demo/ExcelProject/ExcelMain.java
+++ src/main/java/com/demo/ExcelProject/ExcelMain.java
@@ -20,15 +20,26 @@ public class ExcelMain {
 		Workbook xlsxWorkbook=new XSSFWorkbook();
 		Sheet sheet1 = xlsxWorkbook.createSheet("Test");
 		Row row1 = sheet1.createRow(0);
-		row1.createCell(0).setCellValue("Header 1");
-		row1.createCell(1).setCellValue("Header 2");
-		row1.createCell(2).setCellValue("Header ");
+		row1.createCell(0).setCellValue("Team Leader");
+		row1.createCell(1).setCellValue("Representor 1");
+		row1.createCell(2).setCellValue("Representer 2 ");
 		
 		Row row2 = sheet1.createRow(1);
-		row2.createCell(0).setCellValue("Header 1");
-		row2.createCell(1).setCellValue("Header 2");
-		row2.createCell(2).setCellValue("Header ");
+		row2.createCell(0).setCellValue("Aslam");
+		row2.createCell(1).setCellValue("Zaheer");
+		row2.createCell(2).setCellValue("Rahaman");
 		
+//		singleRowElement = new String[row.]
+//		
+//		String  [][] = new String [2][3];
+//		 
+//		for(String i []:Row) {
+//
+		//for (String j:i) {
+			//	System.out.print(j+"   ");
+			//}
+			System.out.println();
+		}
 		
 		xlsxWorkbook.write(new FileOutputStream("ExcelSample.xlsx"));
 		System.out.println("Excel Created SuccesFully");
